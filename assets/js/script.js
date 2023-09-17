1814b0158b2e6cd630691e1500634e7b87010a93
chihiro0525 <qianxunganye@gmail.com>
2023-09-17 22:28:43 +0900

    ヘッダーと紹介文　紹介文だけはスマホ対応になってるけど文字の大きさがおかしい　ヘッダーに関してはスマホ対応になってない

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// ヘッダー・ボタンの要素を取得
+const header = document.getElementById("js-header");
+const button = document.getElementById("js-headerButton");
+
+// ボタンをクリックした時の処理
+button.addEventListener("click", () => {
+  header.classList.toggle("is-open");
+});
